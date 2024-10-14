b48f392ac9bf6e97da014dbe2668bcc222f813f7
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2024-10-14 17:09:57 -0600

    Deletes storyboard

---
+++ FourInARow/FourInARow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FourInARow
+//
+//  Created by David Ali on 14/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
