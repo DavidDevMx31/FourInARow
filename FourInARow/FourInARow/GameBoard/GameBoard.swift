04ffd864c1195d050e4b55a8f5319946d7f2316a
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2024-10-15 14:42:44 -0600

    Adds functions to check wins

--- FourInARow/FourInARow/GameBoard/GameBoard.swift
+++ FourInARow/FourInARow/GameBoard/GameBoard.swift
@@ -59,4 +59,128 @@ class GameBoard: NSObject {
         }
         return true
     }
+    
+    func playerWins(column: Int, row: Int) -> Bool {
+        let chip = currentPlayer.chip
+        if checkVerticalRun(chip: chip, row: row, column: column) {
+            return true
+        }
+        else if checkHorizontalRun(chip: chip, row: row, column: column) {
+            return true
+        }
+        else if checkDiagonalRun(chip: chip, row: row, column: column) {
+            return true
+        }
+        else if checkInverseDiagonalRun(chip: chip, row: row, column: column) {
+            return true
+        }
+        return false
+    }
+    
+    private func checkVerticalRun(chip: Chip, row: Int, column: Int) -> Bool {
+        debugPrint("checkVerticalRun")
+        if row <= 2 { return false }
+        var counter = 1, x = row
+        while x > 0, counter < 4 {
+            if chip != getChip(inColumn: column, row: x - 1) {
+                return false
+            } else {
+                counter += 1
+                x -= 1
+            }
+        }
+        return counter == 4
+    }
+    
+    private func checkHorizontalRun(chip: Chip, row: Int, column: Int) -> Bool {
+        var searchLeft = true, searchRight = true
+        var counter = 1
+        var xOffset = 1
+        
+        while searchLeft, counter < 4, column - xOffset >= 0 {
+            if chip == getChip(inColumn: column - xOffset, row: row) {
+                counter += 1
+                xOffset += 1
+            } else {
+                searchLeft = false
+            }
+        }
+        xOffset = 1
+        while searchRight, counter < 4, column + xOffset <= GameBoard.width - 1 {
+            if chip == getChip(inColumn: column + xOffset, row: row) {
+                xOffset += 1
+                counter += 1
+            }
+            else {
+                searchRight = false
+            }
+        }
+        
+        return counter == 4
+    }
+    
+    private func checkDiagonalRun(chip: Chip, row: Int, column: Int) -> Bool {
+        var searchLeftUp = true, searchRightDown = true, counter = 1, xOffset = 1, yOffset = 1
+        
+        while searchLeftUp, column - xOffset >= 0, row + yOffset <= GameBoard.height - 1,
+                counter < 4  {
+            if chip == getChip(inColumn: column - xOffset, row: row + yOffset) {
+                xOffset += 1
+                yOffset += 1
+                counter += 1
+            }
+            else {
+                searchLeftUp = false
+            }
+        }
+        xOffset = 1
+        yOffset = 1
+        
+        while searchRightDown, column + xOffset <= GameBoard.width - 1, row - yOffset >= 0,
+                counter < 4 {
+            if chip == getChip(inColumn: column + xOffset, row: row - yOffset) {
+                xOffset += 1
+                yOffset += 1
+                counter += 1
+            }
+            else {
+                searchRightDown = false
+            }
+        }
+        
+        return counter == 4
+    }
+    
+    private func checkInverseDiagonalRun(chip: Chip, row: Int, column: Int) -> Bool {
+        var searchUp = true, searchDown = true, counter = 1, xOffset = 1, yOffset = 1
+        
+        while searchDown, column - xOffset >= 0, row - yOffset >= 0, counter < 4 {
+            if chip == getChip(inColumn: column - xOffset, row: row - yOffset) {
+                xOffset += 1
+                yOffset += 1
+                counter += 1
+            }
+            else {
+                searchDown = false
+            }
+        }
+        
+        xOffset = 1
+        yOffset = 1
+        
+        while searchUp, column + xOffset <= GameBoard.width - 1,
+                row + yOffset <= GameBoard.height - 1,
+              counter < 4  {
+            if chip == getChip(inColumn: column + xOffset, row: row + yOffset) {
+                xOffset += 1
+                yOffset += 1
+                counter += 1
+            }
+            else {
+                searchUp = false
+            }
+        }
+        
+        return counter == 4
+    }
 }
