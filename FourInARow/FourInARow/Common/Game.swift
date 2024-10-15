bb4e55848b3ae604f6d5052022974cc630102ad5
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2024-10-15 14:54:50 -0600

    Adds logic to track wins

--- FourInARow/FourInARow/Common/Game.swift
+++ FourInARow/FourInARow/Common/Game.swift
@@ -8,5 +8,16 @@
 import Foundation
 
 class Game {
+    var redWins: Int = 0
+    var yellowWins: Int = 0
     
+    func resetVictoryCounter() {
+        redWins = 0
+        yellowWins = 0
+    }
+    
+    func declareWinner(player: Player) {
+        if player.chip == .red { redWins += 1 }
+        else { yellowWins += 1 }
+    }
 }
